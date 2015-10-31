1c4345e8c90484263006996d2d6858b27fbc29c5
ElliottOlson <olsonelliott@me.com>
2015-10-30 21:45:45 -0500

    Added game countdown and voting method.

--- src/main/java/me/elliottolson/bowspleef/game/Game.java
+++ src/main/java/me/elliottolson/bowspleef/game/Game.java
@@ -1,8 +1,14 @@
 package me.elliottolson.bowspleef.game;
 
+import me.elliottolson.bowspleef.BowSpleef;
+import me.elliottolson.bowspleef.api.GameCountdownEvent;
+import me.elliottolson.bowspleef.api.GameJoinEvent;
+import me.elliottolson.bowspleef.api.GameSpectateEvent;
+import me.elliottolson.bowspleef.api.GameVoteEvent;
 import me.elliottolson.bowspleef.manager.ConfigurationManager;
 import me.elliottolson.bowspleef.manager.PlayerManager;
 import me.elliottolson.bowspleef.util.MessageManager;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
@@ -30,7 +36,8 @@ public class Game {
     private int minimumPlayers;
 
     private Location lobby;
-    private List<Location> spawns;
+    private Location spawn;
+    private Location spectatorSpawn;
 
     public Game(String name) {
         this.name = name;
@@ -84,11 +91,76 @@ public class Game {
 
             player.teleport(lobby);
 
+            GameJoinEvent event = new GameJoinEvent(player, this);
+            Bukkit.getServer().getPluginManager().callEvent(event);
 
+            players.add(player);
+
+            //TODO: Stats, Achievements, Kits, Economy
+
+            for (Player p : players){
+                MessageManager.msg(MessageManager.MessageType.SUCCESS, p, player.getName() + ChatColor.AQUA +
+                        " has joined the game! " + ChatColor.GRAY + "(" + ChatColor.YELLOW + players.size() +
+                        ChatColor.GRAY + "/" + ChatColor.YELLOW + maximumPlayers + ChatColor.GRAY + ")");
+                //TODO: Update scoreboards
+            }
+
+            for (Player p : spectators){
+                MessageManager.msg(MessageManager.MessageType.SUCCESS, p, player.getName() + ChatColor.AQUA +
+                        " has joined the game! " + ChatColor.GRAY + "(" + ChatColor.YELLOW + players.size() +
+                        ChatColor.GRAY + "/" + ChatColor.YELLOW + maximumPlayers + ChatColor.GRAY + ")");
+                //TODO: Update scoreboards
+            }
+
+            //TODO: Update signs
 
         } else if (getState() == GameState.INGAME){
 
+            int gm = player.getGameMode().getValue();
+            double health = player.getHealth();
+            int food = player.getFoodLevel();
 
+            ConfigurationManager.getPlayerConfig().set(player.getName() + ".return.gamemode", gm);
+            ConfigurationManager.getPlayerConfig().set(player.getName() + ".return.health", health);
+            ConfigurationManager.getPlayerConfig().set(player.getName() + ".return.food", food);
+
+            player.setGameMode(GameMode.ADVENTURE);
+            player.setHealth(player.getMaxHealth());
+            player.setFoodLevel(20);
+
+            Location returnLocation = player.getLocation();
+            ConfigurationManager.getPlayerConfig().set(player.getName() + ".return.x", returnLocation.getBlockX());
+            ConfigurationManager.getPlayerConfig().set(player.getName() + ".return.y", returnLocation.getBlockY());
+            ConfigurationManager.getPlayerConfig().set(player.getName() + ".return.z", returnLocation.getBlockZ());
+            ConfigurationManager.getPlayerConfig().set(player.getName() + ".return.world", returnLocation.getWorld().getName());
+
+            PlayerManager.saveInventory(player);
+
+            player.getInventory().clear();
+            player.updateInventory();
+
+            player.teleport(spectatorSpawn);
+
+            GameSpectateEvent event = new GameSpectateEvent(player, this);
+            Bukkit.getServer().getPluginManager().callEvent(event);
+
+            spectators.add(player);
+
+            //TODO: Stats, Achievements, Kits, Economy
+
+            for (Player p : players){
+                MessageManager.msg(MessageManager.MessageType.SUCCESS, p, player.getName() + ChatColor.AQUA +
+                        " is spectating this game!");
+                //TODO: Update scoreboards
+            }
+
+            for (Player p : spectators){
+                MessageManager.msg(MessageManager.MessageType.SUCCESS, p, player.getName() + ChatColor.AQUA +
+                        " is spectating this game!");
+                //TODO: Update scoreboards
+            }
+
+            //TODO: Update signs
 
         } else {
             MessageManager.msg(MessageManager.MessageType.ERROR, player, "You cannot join the game at this time.");
@@ -100,6 +172,54 @@ public class Game {
     }
 
     public void vote(Player player){
+        if (!player.hasPermission("bowspleef.player.vote")){
+            MessageManager.msg(MessageManager.MessageType.ERROR, player, "You do not have permission to vote.");
+            return;
+        }
+
+        if (GameManager.getInstance().getPlayerGame(player) == null){
+            MessageManager.msg(MessageManager.MessageType.ERROR, player, "You must be in a game to vote.");
+            return;
+        }
+
+        if (getState() != GameState.LOBBY){
+            MessageManager.msg(MessageManager.MessageType.ERROR, player, "You are unable to vote at this time.");
+            return;
+        }
+
+        if (voters.contains(player)){
+            MessageManager.msg(MessageManager.MessageType.ERROR, player, "You have already voted once.");
+            return;
+        }
+
+        voters.add(player);
+        MessageManager.msg(MessageManager.MessageType.INFO, player, "You have voted to start the game.");
+
+        {
+            GameVoteEvent event = new GameVoteEvent(player, this);
+            Bukkit.getServer().getPluginManager().callEvent(event);
+        }
+
+        if (players.size() >= 2){
+
+            int votesNeeded = Math.round(players.size() * (2/3));
+            int remaining = votesNeeded - voters.size();
+
+            if (remaining == 0){
+
+                state = GameState.STARTING;
+
+                {
+                    GameCountdownEvent event = new GameCountdownEvent(this);
+                    Bukkit.getServer().getPluginManager().callEvent(event);
+                }
+
+                new GameCountdown(this).runTaskTimer(BowSpleef.getInstance(), 0L, 20L);
+
+                msgAll(MessageManager.MessageType.INFO, "This game will begin soon...");
+            }
+
+        }
 
     }
 
@@ -127,23 +247,33 @@ public class Game {
 
     }
 
+    public void msgAll(MessageManager.MessageType type, String message){
+        for (Player player : getPlayers()){
+            player.sendMessage(type.getPrefix() + ChatColor.GRAY + message);
+        }
+        for (Player player : getSpectators()){
+            player.sendMessage(type.getPrefix() + ChatColor.GRAY + message);
+        }
+    }
+
     public enum GameState {
-        DISABLED("Disabled", 0, ChatColor.DARK_RED),
-        LOADING("Loading", 1, ChatColor.GOLD),
-        LOBBY("Lobby", 2, ChatColor.GREEN),
-        STARTING("Starting", 3, ChatColor.DARK_GREEN),
-        SPREAD("Spreading Out", 4, ChatColor.RED),
-        INGAME("InGame", 5, ChatColor.DARK_RED),
-        RESETTING("Resetting", 6, ChatColor.DARK_RED),
-        INACTIVE("Inactive", 7, ChatColor.DARK_RED),
-        NOTSETUP("Not Setup", 8, ChatColor.DARK_RED),
-        ERROR("Error", 9, ChatColor.DARK_RED);
+        DISABLED("Disabled", 0, ChatColor.DARK_RED, 0),
+        LOADING("Loading", 1, ChatColor.GOLD, 0),
+        LOBBY("Lobby", 2, ChatColor.GREEN, 0),
+        STARTING("Starting", 3, ChatColor.DARK_GREEN, 15),
+        SPREAD("Spreading Out", 4, ChatColor.RED, 5),
+        INGAME("InGame", 5, ChatColor.DARK_RED, 0),
+        RESETTING("Resetting", 6, ChatColor.DARK_RED, 0),
+        INACTIVE("Inactive", 7, ChatColor.DARK_RED, 0),
+        NOTSETUP("Not Setup", 8, ChatColor.DARK_RED, 0),
+        ERROR("Error", 9, ChatColor.DARK_RED, 0);
 
         private String name;
         private int id;
         private ChatColor color;
+        private int time;
 
-        GameState(String name, int id, ChatColor color) {
+        GameState(String name, int id, ChatColor color, int time) {
             this.name = name;
             this.id = id;
             this.color = color;
@@ -160,6 +290,10 @@ public class Game {
         public ChatColor getColor() {
             return color;
         }
+
+        public int getTime() {
+            return time;
+        }
     }
 
     public String getName() {
@@ -168,6 +302,10 @@ public class Game {
 
     public GameState getState() {
         return state;
+    }
+
+    public void setState(GameState state) {
+        this.state = state;
     }
 
     public List<Player> getPlayers() {
---
+++ src/main/java/me/elliottolson/bowspleef/game/GameCountdown.java
@@ -0,0 +1,71 @@
+package me.elliottolson.bowspleef.game;
+
+import me.elliottolson.bowspleef.util.MessageManager;
+import org.bukkit.ChatColor;
+import org.bukkit.scheduler.BukkitRunnable;
+
+/**
+ * Copyright Elliott Olson (c) 2015. All Rights Reserved.
+ * Any code contained within this document, and any associated APIs with similar brandings
+ * are the sole property of Elliott Olson. Distribution, reproduction, taking snippits, or
+ * claiming any contents as your own will break the terms of the license, and void any
+ * agreements with you, the third party.
+ */
+public class GameCountdown extends BukkitRunnable {
+
+    private Game game;
+
+    public GameCountdown(Game game){
+        this.game = game;
+    }
+
+    public void run() {
+
+        int id = game.getState().getId();
+        int time = game.getState().getTime();
+
+        if (id == game.getState().STARTING.getId()){
+
+            if (time == 15 || time == 10 || time == 5 || time <= 3 && time > 0){
+                game.msgAll(MessageManager.MessageType.INFO, "You will be teleported in " + ChatColor.DARK_AQUA +
+                        time + ChatColor.GRAY + " seconds...");
+            }
+
+            if (time == 0){
+                //TODO: Teleport players
+                game.setState(Game.GameState.SPREAD);
+            }
+
+        }
+
+        else if (id == game.getState().SPREAD.getId()){
+
+            if (time == 10){
+                game.msgAll(MessageManager.MessageType.INFO, "You will receive your kits in " + ChatColor.DARK_AQUA +
+                        "10" + ChatColor.GRAY + " seconds...");
+                game.msgAll(MessageManager.MessageType.SUB_INFO, "The game will start momentarily...");
+            }
+
+            if (time == 5 || time <= 3 && time > 1){
+                game.msgAll(MessageManager.MessageType.INFO, "The game will start in " + ChatColor.DARK_AQUA +
+                        time + ChatColor.GRAY + " seconds...");
+            }
+
+            if (time == 0){
+                //TODO: Give Kits
+                game.setState(Game.GameState.INGAME);
+            }
+
+        }
+
+        else if (id == game.getState().INGAME.getId()){
+            cancel();
+        }
+
+        if (time > 0){
+            time--;
+        }
+
+
+    }
+}
